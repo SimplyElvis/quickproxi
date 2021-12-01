f999ae09d502ab5f445b0d9b10e1415d039fcb32
Simply Elvis <provictor.ie@gmail.com>
2021-12-01 16:33:18 +0100

    Code clean up.

--- controllers/weather.controller.js
+++ controllers/weather.controller.js
@@ -23,24 +23,3 @@ const qCityWeather = async (req, res) => {
 };
 
 module.exports = { qCityWeather };
-
-// weatherRoute.get("/api/weather", cache("1 minutes"), async (req, res) => {
-//   try {
-//     const params = new URLSearchParams({
-//       [process.env.OWEATHER_APP_ID]: process.env.OWEATHER_API_KEY,
-//       ...url.parse(req.url, true).query, //spread all params passed as query arguments.
-//     });
-
-//     const OweatherResp = await needle(
-//       "get",
-//       `${process.env.OWEATHER_BASE_URL}?${params}`
-//     );
-//     const OweatherData = OweatherResp.body;
-
-//     res.status(200).json(OweatherData);
-//   } catch (error) {
-//     res.status(500).json(error);
-//   }
-// });
-
-// module.exports = weatherRoute;
--- routes/weather.js
+++ routes/weather.js
@@ -7,21 +7,3 @@ let cache = apiCache.middleware;
 weatherData.get("/api/weather", cache("1 minutes"), qCityWeather);
 
 module.exports = { weatherData };
-// weatherRoute.get("/api/weather", cache("1 minutes"), async (req, res) => {
-//   try {
-//     const params = new URLSearchParams({
-//       [process.env.OWEATHER_APP_ID]: process.env.OWEATHER_API_KEY,
-//       ...url.parse(req.url, true).query, //spread all params passed as query arguments.
-//     });
-
-//     const OweatherResp = await needle(
-//       "get",
-//       `${process.env.OWEATHER_BASE_URL}?${params}`
-//     );
-//     const OweatherData = OweatherResp.body;
-
-//     res.status(200).json(OweatherData);
-//   } catch (error) {
-//     res.status(500).json(error);
-//   }
-// });
